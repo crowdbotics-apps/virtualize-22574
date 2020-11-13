f802620d2c2a65ba90f5bb4e1b18c5034ffe948c
Crowdbotics <team@crowdbotics.com>
2020-11-13 02:03:07 +0000

    Initial commit.

---
+++ ios/virtualize_22574Tests/virtualize_22574Tests.m
@@ -0,0 +1,30 @@
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+#import <React/RCTLog.h>
+#import <React/RCTRootView.h>
+
+#define TIMEOUT_SECONDS 600
+#define TEXT_TO_LOOK_FOR @"Welcome to React"
+
+@interface virtualize_22574 : XCTestCase
+
+@end
+
+@implementation virtualize_22574
+
+- (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
+{
+  if (test(view)) {
+    return YES;
+  }
+  for (UIView *subview in [view subviews]) {
+    if ([self findSubviewInView:subview matching:test]) {
+      return YES;
+    }
+  }
+  return NO;
+}
+
+
+@end
